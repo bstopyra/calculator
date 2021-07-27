c9443b58cc9ce1e9032d0cfcf1ca904a19ff9f7b
ogierhaq <ogierhaq@gmail.com>
2021-07-27 13:57:10 +0200

    finished app

--- scripts.js
+++ scripts.js
@@ -1,6 +1,41 @@
 let operator = "";
 let num1 = 0;
 let num2 = 0;
+const operators = "+-*/%";
+
+const resultBar = document.getElementById("resultNum");
+
+const removeTransition = (e) => {
+  if (e.propertyName !== "transform") return;
+  e.target.classList.remove("playing");
+};
+
+window.addEventListener("keydown", (e) => {
+  let keyDown = e.key;
+  const key = document.querySelector(`button[key='${keyDown}']`);
+  key.classList.add("clicked");
+  if (keyDown === "Enter") {
+    num2 = resultBar.value;
+    operate(+num1, operator, +num2);
+  } else if (operators.includes(keyDown)) {
+    operatorHandler(keyDown);
+  } else if (keyDown === "Escape") {
+    clearFunc();
+  } else if (keyDown === "." || keyDown === ",") {
+    decimalCheck(keyDown);
+  } else if (keyDown === "Backspace") {
+    backspaceFunc();
+  } else if (resultBar.value === "0") {
+    if (parseInt(keyDown, 10)) {
+      resultBar.value = keyDown;
+    }
+  } else if (parseInt(keyDown, 10)) {
+    resultBar.value += keyDown;
+  }
+  setTimeout(() => {
+    key.classList.remove("clicked");
+  }, 200);
+});
 
 const addFunc = (a, b) => {
   return a + b;
@@ -15,78 +50,102 @@ const multiplyFunc = (a, b) => {
 };
 
 const divideFunc = (a, b) => {
-  return a / b;
+  return b !== 0 ? a / b : "ure an idiot";
+};
+
+const modalFunc = (a, b) => {
+  return a % b;
 };
 
 const operate = (a, operator, b) => {
   switch (operator) {
     case "+":
-        console.log(a, b, operator)
-      resultBar.value = addFunc(a, b);
+      handleOperatorCase(addFunc(a, b));
       break;
     case "-":
-      resultBar.value = subtractFunc(a, b);
+      handleOperatorCase(subtractFunc(a, b));
       break;
     case "*":
-      resultBar.value = multiplyFunc(a, b);
+      handleOperatorCase(multiplyFunc(a, b));
       break;
     case "/":
-      resultBar.value = divideFunc(a, b);
+      handleOperatorCase(divideFunc(a, b));
+      break;
+    case "%":
+      handleOperatorCase(modalFunc(a, b));
+      break;
+    default:
+      resultBar.value = "lmao";
       break;
   }
-
-  // CODE FOR DOING EVERY CALCULATION IN ONE STEP
-  //   let numbers = resultBar.value.split(/[^0-9]/g);
-  //   let sum = +numbers[0];
-  //   console.log(sum)
-  //   for (let i = 1; i < numbers.length; i++) {
-  //     switch (operator[i - 1]) {
-  //       case "+":
-  //         sum = addFunc(sum, +numbers[i]);
-  //         break;
-  //       case "-":
-  //         sum = subtractFunc(sum, +numbers[i]);
-  //         break;
-  //       case "*":
-  //         sum = multiplyFunc(sum, +numbers[i]);
-  //         break;
-  //       case "/":
-  //         sum = divideFunc(sum, +numbers[i]);
-  //         break;
-  //       default:
-  //         return "error";
-  //     }
-  //   }
-  //   resultBar.value = sum;
-  //   operator = "";
 };
 
-const resultBar = document.getElementById("resultNum");
+const calculatorHandler = (button) => {
+  if (button === "=") {
+    num2 = resultBar.value;
+    operate(+num1, operator, +num2);
+  } else if (operators.includes(button)) {
+    operatorHandler(button);
+  } else if (button === "clear") {
+    clearFunc();
+  } else if (button === "+/-") {
+    negativeValueFunc();
+  } else if (button === ".") {
+    decimalCheck(button);
+  } else if (button === "backspace") {
+    backspaceFunc();
+  } else if (resultBar.value === "0") {
+    resultBar.value = button;
+  } else {
+    resultBar.value += button;
+  }
+};
+
+const handleOperatorCase = (calcFunc) => {
+  resultBar.value = calcFunc;
+  if (resultBar.value.split(".")[1]) {
+    resultBar.value = roundDecimals(resultBar.value);
+  }
+};
+
+const roundDecimals = (num) => {
+  const dec = num.split(".")[1];
+  const len = dec && dec.length > 3 ? 3 : dec.length;
+  return Number(num).toFixed(len);
+};
+
+const decimalCheck = (button) => {
+  if (!resultBar.value.includes(".")) {
+    resultBar.value += button;
+  }
+};
+
+const clearFunc = () => {
+  resultBar.value = "0";
+  num1 = 0;
+  num2 = 0;
+};
+
+const operatorHandler = (button) => {
+  num1 = resultBar.value;
+  operator = button;
+  resultBar.value = 0;
+};
+
+const backspaceFunc = () => {
+  resultBar.value = resultBar.value.substring(0, resultBar.value.length - 1);
+};
+
+const negativeValueFunc = () => {
+  console.log(resultBar.value);
+  if (resultBar.value != "0") {
+    resultBar.value = 0 - resultBar.value;
+  }
+};
 
 const buttons = document.querySelectorAll("button");
-
 buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    if (button.id === "=") {
-      num2 = resultBar.value;
-      operate(+num1, operator, +num2);
-    } else if (
-      button.id === "+" ||
-      button.id === "-" ||
-      button.id === "*" ||
-      button.id === "/"
-    ) {
-      num1 = resultBar.value;
-      operator = button.id;
-      resultBar.value = 0;
-    } else if (button.id === "clear") {
-      resultBar.value = "0";
-      num1 = 0;
-      num2 = 0;
-    } else if (resultBar.value === "0") {
-      resultBar.value = button.id;
-    } else {
-      resultBar.value += button.id;
-    }
+  button.addEventListener("click", (e) => {
+    calculatorHandler(button.id);
   });
 });
